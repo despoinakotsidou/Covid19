4cad15af811e375e33694d111dc5285f4b4f0efa
Despi <dkotsidou@gmail.com>
2025-08-27 21:34:35 +0200

    more changes

--- covid.sql
+++ covid.sql
@@ -12,4 +12,19 @@ order by 1,2
 
 Select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage 
 From PortfolioProject.. CovidDeaths
+Where location like '%austria'
 order by 1,2
+
+-- Looking at Total Cases vs Population 
+
+Select location, date, total_cases, population, (total_cases/population)*100 as PercentPopulationInfected
+From PortfolioProject.. CovidDeaths
+Where location like '%austria'
+order by 1,2
+
+--Looking at Countries Highest Inflation Rate compared to Population 
+Select location, population, MAX(total_cases) as HighestInfectionCount, MAX((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject.. CovidDeaths
+--Where location like '%austria'
+GROUP by location, population
+order by PercentPopulationInfected desc
