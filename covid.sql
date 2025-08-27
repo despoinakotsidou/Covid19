febe7dd2729fe11e0828d73b4b82911a0feaa870
Despi <dkotsidou@gmail.com>
2025-08-27 20:31:34 +0200

    new project

---
+++ covid.sql
@@ -0,0 +1,11 @@
+Select *
+From PortfolioProject.. CovidDeaths
+order by 3,4
+
+Select *
+From PortfolioProject.. CovidVaccinations
+order by 3,4
+
+Select location, date, total_cases, new_cases, total_deaths, population 
+From PortfolioProject.. CovidDeaths
+order by 1,2
