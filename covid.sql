88b648270102b4da0f0a1d05d6b4a50b888889c9
Despi <dkotsidou@gmail.com>
2025-08-27 21:00:16 +0200

    more changes

--- covid.sql
+++ covid.sql
@@ -9,3 +9,7 @@ order by 3,4
 Select location, date, total_cases, new_cases, total_deaths, population 
 From PortfolioProject.. CovidDeaths
 order by 1,2
+
+Select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage 
+From PortfolioProject.. CovidDeaths
+order by 1,2
