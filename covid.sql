34d97c2b18f0854c2736195fd6869897fd457bcc
Despi <dkotsidou@gmail.com>
2025-08-28 11:22:26 +0200

    add more changes

--- covid.sql
+++ covid.sql
@@ -1,5 +1,6 @@
 Select *
 From PortfolioProject.. CovidDeaths
+Where continent is not NULL
 order by 3,4
 
 Select *
@@ -28,3 +29,47 @@ From PortfolioProject.. CovidDeaths
 --Where location like '%austria'
 GROUP by location, population
 order by PercentPopulationInfected desc
+
+
+-- Showing Countries with hoghest DeathCount per Population 
+Select location, MAX(cast(total_deaths as int)) as TotalDeathCount
+From PortfolioProject.. CovidDeaths
+--Where location like '%austria'
+Where continent is not NULL
+GROUP by location
+order by TotalDeathCount desc
+
+-- BY CONTINENT
+
+
+-- Showing the Continent with highest death count per populatuon
+
+Select continent, MAX(cast(total_deaths as int)) as TotalDeathCount
+From PortfolioProject.. CovidDeaths
+--Where location like '%austria'
+Where continent is not NULL
+GROUP by continent
+order by TotalDeathCount desc
+
+--BY GLOBAL 
+
+Select date, SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, 
+SUM(cast(new_deaths as int))/SUM(new_cases)*100 AS DaethPercentage  
+From PortfolioProject.. CovidDeaths
+--Where location like '%austria'
+Where continent is not NULL
+GROUP by date
+order by 1,2
+
+--Join the Tbles 
+
+-- Looking at Total Population vs Vacination 
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+    On dea.location = vac.location 
+    and dea.date = vac.date 
+Where dea.continent is not NULL
+order by 2,3
+
